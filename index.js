46b140629945dc863f2176997bc7e9febb59ce29
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-01-04 16:01:09 +0100

    Feladat teljesítve implementálva

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+let todoLista = [];
+
+function megjelenites() {
+    let lista = document.getElementById("lista");
+    lista.innerHTML = "";
+    todoLista.forEach(function(element) {
+        let li = document.createElement('li');
+        li.innerHTML = element.text;
+        let input = document.createElement("input");
+        input.type = "checkbox";
+        input.checked = element.done == true || element.done == '1';
+
+        input.addEventListener('input', function(event) {
+            let todoItem = {
+                id: element.id,
+                text: element.text,
+                done: !(element.done == true || element.done == '1'),
+            };
+            fetch('http://localhost:8080/todos/' + todoItem.id, {
+                method: 'PUT',
+                body: JSON.stringify(todoItem)
+            }).then(function(response) {
+                if (response.ok) {
+                    element.done = todoItem.done;
+                }
+                megjelenites();
+            }).catch(function(error) {
+                window.alert("Hiba!");
+                megjelenites();
+            });
+        });
+
+        li.prepend(input);
+        lista.appendChild(li);
+    });
+}
+
 window.addEventListener('DOMContentLoaded', function() {
     fetch('http://localhost:8080/todos')
         .then(function(response) {
@@ -7,13 +44,9 @@ window.addEventListener('DOMContentLoaded', function() {
                 return response.json();
             }
         }).then(function(eredmeny) {
+            todoLista = eredmeny;
             console.log(eredmeny);
-            let lista = document.getElementById("lista");
-            eredmeny.forEach(function(element) {
-                let li = document.createElement('li');
-                li.innerHTML = element.text;
-                lista.appendChild(li);
-            });
+            megjelenites();
         }).catch(function(error) {
             window.alert("Hiba! " + error.message);
         });
@@ -24,11 +57,14 @@ window.addEventListener('DOMContentLoaded', function() {
                 text: todoText,
                 done: false,
             };
+            document.getElementById("todoText").value = "";
             fetch('http://localhost:8080/todos', {
                 method: 'POST',
                 body: JSON.stringify(todoItem),
             }).then(function(response) {
                 if (response.ok) {
+                    todoLista.push(todoItem);
+                    megjelenites();
                     window.alert("Siker!");
                 } else {
                     throw new Error(response.status);
